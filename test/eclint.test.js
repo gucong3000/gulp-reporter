c749325f9706bb159deaca1febc178cee50d42a3
刘祺 <qil@jumei.com>
2017-03-27 18:12:48 +0800

    update dev dependencies

--- test/eclint.test.js
+++ test/eclint.test.js
@@ -2,21 +2,23 @@
 const describe = require('mocha').describe;
 const it = require('mocha').it;
 const assert = require('assert');
-const vfs = require('vinyl-fs');
 const gutil = require('gulp-util');
+const vfs = require('vinyl-fs');
 const eclint = require('eclint');
 const reporter = require('../');
 
 require('./sandbox');
 
-describe.skip('ECLint', function() {
+describe('ECLint', function() {
 	it('console reporter', function(done) {
 		return vfs.src('test/fixtures/eclint/invalid.js', {
 			base: process.cwd(),
 			stripBOM: false,
 		})
 			.pipe(eclint.check())
-			.pipe(reporter()).on('error', ex => {
+			.pipe(reporter({
+				filter: null
+			})).on('error', ex => {
 				assert.equal(ex.plugin, 'gulp-reporter');
 				assert.equal(ex.message, 'Lint failed for: test/fixtures/eclint/invalid.js');
 
