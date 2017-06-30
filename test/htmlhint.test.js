7d2b7668443b8c7bfe212cecd4f19d1f183c306f
刘祺 <qil@jumei.com>
2017-06-30 14:54:13 +0800

    support short url of t.cn

--- test/htmlhint.test.js
+++ test/htmlhint.test.js
@@ -4,21 +4,12 @@ const gutil = require('gulp-util');
 const vfs = require('vinyl-fs');
 const htmlhint = require('gulp-htmlhint');
 const reporter = require('../');
+const locale = require('../lib/locale');
 
 require('./sandbox');
 
 describe('HTMLHint', () => {
 	require('../lib/locale');
-	const localeModule = require.cache[require.resolve('../lib/locale')];
-
-	before(() => {
-		localeModule.exports = 'zh_CN';
-		delete require.cache[require.resolve('../lib/lint-error')];
-	});
-
-	after(() => {
-		localeModule.exports = 'en_US';
-	});
 
 	it('console reporter', (done) => {
 		return vfs.src('test/fixtures/htmlhint/invalid.html', {
@@ -31,11 +22,12 @@ describe('HTMLHint', () => {
 				assert.equal(ex.plugin, 'gulp-reporter');
 				assert.equal(ex.message, 'Lint failed for: test/fixtures/htmlhint/invalid.html');
 				assert.ok(/^test\/fixtures\/htmlhint\/invalid.html\n/.test(gutil.log.lastCall.args[0]));
-				assert.ok(gutil.log.lastCall.args[0].indexOf('(HTMLHint doctype-first https://goo.gl/jcpmfT)') >= 0);
-				assert.ok(gutil.log.lastCall.args[0].indexOf('(HTMLHint tag-pair https://goo.gl/wFHTJ5)') >= 0);
-				if (+process.version.replace(/^v?(\d+).+?$/, '$1') >= 5) {
-					assert.ok(gutil.log.lastCall.args[0].indexOf('doctype必须首先声明。') >= 0);
-					assert.ok(gutil.log.lastCall.args[0].indexOf('标签必须匹配，缺失') >= 0);
+				if (locale === 'zh_CN') {
+					assert.ok(gutil.log.lastCall.args[0].indexOf('doctype必须首先声明。 (HTMLHint doctype-first http://t.cn/Ro8MlrW)') >= 0);
+					assert.ok(gutil.log.lastCall.args[0].indexOf('标签必须匹配，缺失：[ </h1> ]，在第7行匹配开始标签[ <h1> ]失败 (HTMLHint tag-pair http://t.cn/Ro8MlgP)') >= 0);
+				} else {
+					assert.ok(gutil.log.lastCall.args[0].indexOf('Doctype must be declared first. (HTMLHint doctype-first https://goo.gl/jcpmfT)') >= 0);
+					assert.ok(gutil.log.lastCall.args[0].indexOf('Tag must be paired, missing: [ </h1> ], start tag match failed [ <h1> ] on line 7. (HTMLHint tag-pair https://goo.gl/wFHTJ5)') >= 0);
 				}
 				done();
 			});
