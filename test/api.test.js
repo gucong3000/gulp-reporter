4ffe307347a3467513fc7797172c7a993f25acad
刘祺 <qil@jumei.com>
2017-07-05 16:53:25 +0800

    add buffered-spawn

--- test/api.test.js
+++ test/api.test.js
@@ -55,7 +55,7 @@ describe('API', () => {
 		assert.equal(result[4].severity, '!');
 	});
 
-	describe('git-author', () => {
+	describe('git precommit env', () => {
 		before(() => {
 			process.env.GIT_AUTHOR_DATE = '@1498558291 +0800';
 			process.env.GIT_AUTHOR_NAME = 'name.test';
@@ -67,12 +67,33 @@ describe('API', () => {
 			delete process.env.GIT_AUTHOR_NAME;
 			delete process.env.GIT_AUTHOR_EMAIL;
 		});
+
 		it('getEvnAuthor()', () => {
 			assert.deepEqual(gitAuthor(), {
 				time: 1498558291,
 				name: 'name.test',
 				email: 'test@test.com'
 			});
+		});
+
+		it('file not in git repo with error', done => {
+			return vfs.src('test/fixtures/eslint/invalid.js', {
+				base: process.cwd()
+			})
+				.pipe(eslint())
+				.pipe(through.obj((file, encoding, done) => {
+					file.path = '/invalid.js';
+					file.cwd = '/';
+					done(null, file);
+				}))
+				.pipe(reporter({
+					author: null,
+					sort: false,
+				}))
+				.on('error', ex => {
+					assert.equal(ex.plugin, 'gulp-reporter');
+					done();
+				});
 		});
 	});
 
@@ -100,185 +121,189 @@ describe('API', () => {
 		}));
 	});
 
-	it('file not in git repo with error', done => {
-		return vfs.src('test/fixtures/eslint/invalid.js', {
-			base: process.cwd()
-		})
-			.pipe(eslint())
-			.pipe(through.obj((file, encoding, done) => {
-				file.path = '/invalid.js';
-				file.cwd = '/';
-				done(null, file);
-			}))
-			.pipe(reporter({
-				author: null,
-				sort: false,
-			})).on('error', ex => {
-				assert.equal(ex.plugin, 'gulp-reporter');
-				done();
-			});
-	});
-
-	describe('git-author', () => {
-		before(() => {
-			process.env.GIT_AUTHOR_DATE = '@1498558291 +0800';
-			process.env.GIT_AUTHOR_NAME = 'name.test';
-			process.env.GIT_AUTHOR_EMAIL = 'test@test.com';
-		});
-
-		after(() => {
-			delete process.env.GIT_AUTHOR_DATE;
-			delete process.env.GIT_AUTHOR_NAME;
-			delete process.env.GIT_AUTHOR_EMAIL;
-		});
-		it('getEvnAuthor()', () => {
-			assert.deepEqual(gitAuthor(), {
-				time: 1498558291,
-				name: 'name.test',
-				email: 'test@test.com'
-			});
-		});
-	});
-
 	it('`options` as callback', done => {
 		return vfs.src('test/fixtures/eslint/*.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter(() => {
-			return {
-				fail: false,
-			};
-		})).on('finish', done).on('error', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter(() => {
+				return {
+					fail: false,
+				};
+			}))
+			.on('finish', done)
+			.on('error', done);
 	});
 
 	it('`options.author` as string (name)', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter(() => {
-			return {
-				author: 'not exist',
-			};
-		})).on('finish', done).on('error', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter(() => {
+				return {
+					author: 'not exist',
+				};
+			}))
+			.on('finish', done)
+			.on('error', done);
 	});
 
 	it('`options.author` as string (email)', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: 'noexist@mail.com',
-		})).on('finish', done).on('error', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: 'noexist@mail.com',
+			}))
+			.on('finish', done)
+			.on('error', done);
 	});
 
 	it('`options.expires` as textual time periods', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: null,
-			expires: '1d',
-		})).on('finish', done).on('error', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: null,
+				expires: '1d',
+			}))
+			.on('finish', done)
+			.on('error', done);
 	});
 
 	it('`options.expires` as ISO 8601 Extended Format', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: null,
-			expires: '2018-01-01T00:00:00.000Z',
-		})).on('finish', done).on('error', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: null,
+				expires: '2018-01-01T00:00:00.000Z',
+			}))
+			.on('finish', done)
+			.on('error', done);
 	});
 
 	it('`options.expires` as number', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: null,
-			expires: Infinity,
-		})).on('error', ex => {
-			assert.equal(ex.message, 'Lint failed for: test/fixtures/eslint/invalid.js');
-			done();
-		});
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: null,
+				expires: Infinity,
+			}))
+			.on('error', ex => {
+				assert.equal(ex.message, 'Lint failed for: test/fixtures/eslint/invalid.js');
+				done();
+			});
 	});
 
 	it('`options.expires` TypeError', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: null,
-			expires: true,
-		})).on('error', ex => {
-			assert.equal(ex.message, '`options.expires` must be `Number`, `Date` or `string`.');
-			done();
-		});
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: null,
+				expires: true,
+			}))
+			.on('error', ex => {
+				assert.equal(ex.message, '`options.expires` must be `Number`, `Date` or `string`.');
+				done();
+			});
 	});
 
 	it('`options.expires` as number TypeError', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: null,
-			expires: -Infinity,
-		})).on('error', ex => {
-			assert.equal(ex.message, '`options.expires` must be greater than 0.');
-			done();
-		});
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: null,
+				expires: -Infinity,
+			}))
+			.on('error', ex => {
+				assert.equal(ex.message, '`options.expires` must be greater than 0.');
+				done();
+			});
 	});
 
 	it('`options.expires` as invalid string TypeError', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: null,
-			expires: 'error',
-		})).on('error', ex => {
-			assert.equal(ex.message, '`options.expires` must be valid `Date`.');
-			done();
-		});
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: null,
+				expires: 'error',
+			}))
+			.on('error', ex => {
+				assert.equal(ex.message, '`options.expires` must be valid `Date`.');
+				done();
+			});
 	});
 
 	it('`options.author.email` as RegExp match nothing', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: {
-				email: /^not_exist@mail.com$/
-			},
-		})).on('finish', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: {
+					email: /^not_exist@mail.com$/
+				},
+			}))
+			.on('finish', done);
 	});
 
 	it('`options.author.email` as RegExp match anything', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: {
-				email: /.+/
-			},
-		})).on('error', ex => {
-			assert.equal(ex.message, 'Lint failed for: test/fixtures/eslint/invalid.js');
-			done();
-		});
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: {
+					email: /.+/
+				},
+			}))
+			.on('error', ex => {
+				assert.equal(ex.message, 'Lint failed for: test/fixtures/eslint/invalid.js');
+				done();
+			});
 	});
 
 	it('`options.author.name` as RegExp match nothing', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: {
-				name: /^not_exist$/
-			},
-		})).on('finish', done);
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: {
+					name: /^not_exist$/
+				},
+			}))
+			.on('finish', done);
 	});
 
 
 	it('`options.author.name` as RegExp match anything', done => {
 		return vfs.src('test/fixtures/eslint/invalid.js', {
 			base: process.cwd(),
-		}).pipe(eslint()).pipe(reporter({
-			author: {
-				name: /.+/
-			},
-		})).on('error', ex => {
-			assert.equal(ex.message, 'Lint failed for: test/fixtures/eslint/invalid.js');
-			done();
-		});
+		})
+			.pipe(eslint())
+			.pipe(reporter({
+				author: {
+					name: /.+/
+				},
+			}))
+			.on('error', ex => {
+				assert.equal(ex.message, 'Lint failed for: test/fixtures/eslint/invalid.js');
+				done();
+			});
 	});
 
 });
