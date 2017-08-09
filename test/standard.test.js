189f643cbe6b82cb2f02274da6170df2be076f6d
刘祺 <qil@jumei.com>
2017-08-09 16:04:57 +0800

    support for standard

---
+++ test/standard.test.js
@@ -0,0 +1,25 @@
+'use strict';
+const assert = require('assert');
+const vfs = require('vinyl-fs');
+const standard = require('gulp-standard');
+const reporter = require('../');
+const sandbox = require('./sandbox');
+
+describe('standard', () => {
+	it('console reporter', done => {
+		return vfs.src('invalid.js', {
+			cwd: 'test/fixtures/standard/'
+		})
+			.pipe(standard())
+			.pipe(reporter({
+				author: null,
+			}))
+			.on('error', ex => {
+				assert.equal(ex.plugin, 'gulp-reporter');
+				assert.equal(ex.message, 'Lint failed for: invalid.js');
+				const result = sandbox.getLog().split(/\s*\r?\n\s*/g);
+				assert.equal(result[0], 'invalid.js');
+				done();
+			});
+	});
+});
