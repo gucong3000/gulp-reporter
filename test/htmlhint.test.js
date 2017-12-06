b19b52ff47c61f0424f6e12bdca896f8f0148749
刘祺 <gucong@gmail.com>
2017-12-06 13:43:27 +0800

    use t.cn when in gfw

--- test/htmlhint.test.js
+++ test/htmlhint.test.js
@@ -22,14 +22,18 @@ describe('HTMLHint', () => {
 				assert.equal(ex.message, 'Lint failed for: test/fixtures/htmlhint/invalid.html');
 				const log = sandbox.getLog();
 				assert.ok(/^test\/fixtures\/htmlhint\/invalid.html\n/.test(log));
-				if (locale === 'zh_CN') {
-					assert.ok(log.indexOf('doctype必须首先声明。 (HTMLHint doctype-first http://t.cn/Ro8MlrW)') >= 0);
-					assert.ok(log.indexOf('标签必须匹配，缺失：[ </h1> ]，在第7行匹配开始标签[ <h1> ]失败 (HTMLHint tag-pair http://t.cn/Ro8MlgP)') >= 0);
-				} else {
-					assert.ok(log.indexOf('Doctype must be declared first. (HTMLHint doctype-first https://goo.gl/jcpmfT)') >= 0);
-					assert.ok(log.indexOf('Tag must be paired, missing: [ </h1> ], start tag match failed [ <h1> ] on line 7. (HTMLHint tag-pair https://goo.gl/wFHTJ5)') >= 0);
+				try {
+					if (locale === 'zh_CN') {
+						assert.ok(log.indexOf('doctype必须首先声明。 (HTMLHint doctype-first http://t.cn/Ro8MlrW)') >= 0);
+						assert.ok(log.indexOf('标签必须匹配，缺失：[ </h1> ]，在第7行匹配开始标签[ <h1> ]失败 (HTMLHint tag-pair http://t.cn/Ro8MlgP)') >= 0);
+					} else {
+						assert.ok(log.indexOf('Doctype must be declared first. (HTMLHint doctype-first http') >= 0);
+						assert.ok(log.indexOf('Tag must be paired, missing: [ </h1> ], start tag match failed [ <h1> ] on line 7. (HTMLHint tag-pair http') >= 0);
+					}
+					done();
+				} catch (ex) {
+					done(ex);
 				}
-				done();
 			});
 	});
 });
